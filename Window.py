c51a8ba17b9855da2c088a1c9d9ee43607ce9145
huangchangchang 00018334 <h00018334@hihonor.com>
2024-07-16 22:05:27 +0800

    add version check function

--- Window.py
+++ Window.py
@@ -6,6 +6,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 
+from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QMessageBox
+
 from Ui_UI import Ui_MainWindow
 from Panel import Panel
 from ADBMonitor import ADBMonitor
@@ -72,9 +76,55 @@ class Window(QtWidgets.QWidget):
 
         json_pro.json_file_init()
 
+        # 创建 QNetworkAccessManager 实例
+        self.manager = QNetworkAccessManager(self)
+        self.manager.finished.connect(self.on_finished)    
+
+        # 检查版本更新
+        self.check_for_updates()    
+
         logger.info('end init\n')
 
         self.MainWindow.show()
+
+    def get_version(self):
+        try:
+            with open("version.txt", 'r') as f:
+                self.local_version = f.readline()
+        except Exception as e:
+            self.local_version = "1.0"
+            logger.exception(e)
+        self.MainWindow.setWindowTitle(f"MainWindow {self.local_version}")
+
+    def check_for_updates(self):
+        self.get_version()
+
+        # 发送 GET 请求获取版本信息
+        url = QUrl("https://raw.githubusercontent.com/mynameishcc/lcd_debug_version/main/version.txt")  # 替换为实际的版本检查URL
+        request = QNetworkRequest(url)
+        self.manager.get(request)
+
+    def on_finished(self, reply):
+        # 处理响应
+        if reply.error():
+            QMessageBox.critical(self, "错误", f"版本检查失败: {reply.errorString()}")
+        else:
+            response = reply.readAll().data().decode()
+            latest_version = response  # 假设响应是一个简单的版本号字符串
+
+            logger.info(f"latest_version: {latest_version}, local_version: {self.local_version}")
+
+            if float(latest_version) > float(self.local_version):
+                self.show_update_dialog(latest_version)
+
+    def show_update_dialog(self, message):
+        msg_box = QMessageBox(self)
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.setWindowTitle("版本更新")
+        msg_box.setText(f"可用版本{message}<br><a href='https://w3.hihonor.com/weshare/community/#/topic-detail?topicId=963616788427227136'>点击这里更新</a>")
+        msg_box.setTextFormat(Qt.RichText)  # 允许使用富文本格式
+        msg_box.setStandardButtons(QMessageBox.Ok)
+        msg_box.exec_()
 
     def dump_code_enable_(self, enable):
         if enable and not self.ui.enable_debug_log_checkbox.isChecked():
